aa9ba35ac95e9502daeb24eeb873165a5db0405b
Greg Akins <angrygreg@gmaail.com>
2011-09-20 22:23:35 -0400

    Because the default route was protected, a new user would get a signon page immediately.. adding a home index (still need to format) for a landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Navigator::Application.routes.draw do
+  get "home/index"
+
   devise_for :users
 
   resources :groups
   resources :contacts
   resources :users
 
-  root :to => "groups#index"
+  root :to => "home#index"
 
   match "incoming_message", :to => "incoming_message#index"
 
