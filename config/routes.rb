6803da9f95b038866e454bd90e617ce935996088
Greg Akins <angrygreg@gmaail.com>
2011-10-29 15:47:13 -0400

    Putting landing page in place, temporarily disabling all routes, including JQuery libraries

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Navigator::Application.routes.draw do
 
-  get "profile/edit"
 
+#   root :to => "beta#index"
+#   get "profile/edit"
+#   match "groups/remove_contact", :to => "groups#remove_contact"
+#   match "users", :to => "users#index", :via => "get"
+#   match "incoming_message", :to => "incoming_message#index"
 
-  match "groups/remove_contact", :to => "groups#remove_contact"
-  match "users", :to => "users#index", :via => "get"
-  match "incoming_message", :to => "incoming_message#index"
+#   post "messages/deliver"
+#  get "home/index"
+#   devise_for :users, :controllers => { :registrations => "registrations" }
+# #  devise_for :users
 
-  post "messages/deliver"
-  get "home/index"
-  devise_for :users, :controllers => { :registrations => "registrations" }
-#  devise_for :users
+#   resources :users
+#   resources :groups
+#   resources :subscriptions
+#   resources :contacts
+#   resources :messages
 
-  resources :users
-  resources :groups
-  resources :subscriptions
-  resources :contacts
-  resources :messages
-
-  root :to => "home#index"
+#   root :to => "home#index"
 
 end
