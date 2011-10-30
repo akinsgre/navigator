0e4b4e120981acb3fd1dc9ce359a86678956815d
Greg Akins <angrygreg@gmaail.com>
2011-10-30 12:11:46 -0400

    Adding email invite form

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Navigator::Application.routes.draw do
 
 
-#   root :to => "beta#index"
-#   get "profile/edit"
-#   match "groups/remove_contact", :to => "groups#remove_contact"
-#   match "users", :to => "users#index", :via => "get"
-#   match "incoming_message", :to => "incoming_message#index"
+   post "email/create"
 
-#   post "messages/deliver"
-#  get "home/index"
-#   devise_for :users, :controllers => { :registrations => "registrations" }
-# #  devise_for :users
+   get "profile/edit"
+   match "groups/remove_contact", :to => "groups#remove_contact"
+   match "users", :to => "users#index", :via => "get"
+   match "incoming_message", :to => "incoming_message#index"
+   post "messages/deliver"
+   get "home/index"
+   devise_for :users, :controllers => { :registrations => "registrations" }
+ #  devise_for :users
 
-#   resources :users
-#   resources :groups
-#   resources :subscriptions
-#   resources :contacts
+   # resources :users
+   # resources :groups
+   # resources :subscriptions
+   # resources :contacts
 #   resources :messages
 
 #   root :to => "home#index"
