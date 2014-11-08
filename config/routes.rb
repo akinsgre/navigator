0309fe27367b3327dfd970de3a10cf1a1d5e746c
Greg Akins <angrygreg@gmail.com>
2014-11-07 20:15:45 -0500

    Re-adding routes that I removed for testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,41 @@
 Navigator::Application.routes.draw do
 
+  get "twiml/say"
+  get "sponsors/info"
+
+  post "email/create"
+  get "email/show"
+  get "profile/edit"
+  get "groups/:id/add_contact", :to => 'groups#add_contact'
+  post "groups/save_contact", :to => "groups#save_contact"
+  get "groups/remove_contact", :to =>  "groups#remove_contact"
+  
+  get "incoming_message", :to => "incoming_message#index"
+
+  post "messages/deliver"
+  get "home/index"
+  get "donate/new"
+
+  #devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
+
+  match "users/sign_out" => "users#sign_out", :via => :get
+
+  resources :users do
+    resources :groups
+    resources :contacts
+    resources :subscriptions
+  end
+
+  resources :subscriptions
+
+  resources :groups do
+    resources :contacts
+  end
+
+
+  resources :messages
+
 
   root :to => "home#index"
 
