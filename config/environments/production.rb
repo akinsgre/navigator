8d0af8ff15ec24a94b62df587a8fd6d122b1202f
Greg Akins <angrygreg@gmail.com>
2014-11-18 10:29:55 -0500

    Adding email configuration to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,18 @@ Navigator::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
   config.eager_load = true
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['EMAIL_NAME'],
+    password:             ENV['EMAIL_PASSWORD']
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
