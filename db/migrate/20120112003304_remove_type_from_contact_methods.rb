46d9c14a3375bc9f754711010cb7e93bb0baa741
Greg Akins <angrygreg@gmail.com>
2014-11-20 07:47:03 -0500

    Removing migration that doesn't seem to matter.

--- db/migrate/20120112003304_remove_type_from_contact_methods.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTypeFromContactMethods < ActiveRecord::Migration
-  def self.up
-    remove_column :contacts, :type
-  end
-
-  def self.down
-    add_column :contacts, :type, :integer
-  end
-end
