d800d3032921134a73ffc6f294339ec6c69e6a30
Greg Akins <angrygreg@gmail.com>
2014-12-22 16:41:45 -0500

    TRying to fix paths for font files (glphicons)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Navigator::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( *.eot *.svg *.ttf *.woff *.otf)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
