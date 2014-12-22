87e5d55a46a166b48bdc1da65fa08e4b52b3b2f1
Greg Akins <angrygreg@gmail.com>
2014-12-22 16:48:31 -0500

    Still trying to fix the fucking glyphicons

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ end
 module Navigator
   class Application < Rails::Application
 
-    #    config.assets.paths << Rails.root.join("vendor","assets", "fonts")
+    config.assets.paths << Rails.root.join("app","assets", "fonts")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
