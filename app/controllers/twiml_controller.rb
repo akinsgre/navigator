951f4c3ff425f764860a3c37d2518e00b5668004
Greg Akins <angrygreg@gmail.com>
2015-01-17 16:31:51 -0500

    Get response before playing message

--- app/controllers/twiml_controller.rb
+++ app/controllers/twiml_controller.rb
@@ -7,5 +7,10 @@ class TwimlController < ApplicationController
     authorized = params[:secret] && ( params[:secret] == ENV['NMC_API_KEY'])
     Rails.logger.info "##### TwiML response = #{@message} and #{@sponsor_msg}"
     render(:file => File.join(Rails.root, 'public/403'), :status => 403, :layout => false, content_type: "text/xml" ) unless authorized
+    if params[:AnsweredBy]
+      render :say
+    else
+      render :human_check
+    end
   end
 end
