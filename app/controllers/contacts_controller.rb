b044c424130fe35acf0936f42ebd83055b9000f1
Greg Akins <angrygreg@gmail.com>
2015-02-02 15:07:07 -0500

    Getting contact from user.contacts instead of Contact.find

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -149,7 +149,7 @@ class ContactsController < ApplicationController
   end
   def find_for_verification
     normalized_entry = Phone.normalize_number(params[:Called], :default_country_number => '01')
-    contact = Contact.find_by_normalized_entry_and_type(normalized_entry, 'Phone')
+    contact = current_user.contacts.where(:normalized_entry => normalized_entry, :type => 'Phone').first
     @token = SecureRandom.urlsafe_base64(nil, false)
     Rails.logger.debug "######## Setting Token = #{@token} for contact #{contact.inspect} ; normalized_entry = #{normalized_entry}"
     $redis.set(@token,contact.id)
