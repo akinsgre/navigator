7bdaedde9f8b584c4e13056d39efb5d249489f31
Greg Akins <angrygreg@gmail.com>
2015-02-23 23:27:10 -0500

    Adding redis config for sidekiq

--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -1,10 +1,12 @@
 require 'redis'
 #'redis://username:password@my.host:6389'
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 $rollout = Rollout.new($redis)
 $rollout.deactivate(:facebook)
+
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 
 
